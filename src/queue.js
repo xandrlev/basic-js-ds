9c8393565dc9ef90b1f45c33f27f550242125970
xandrlev <xandrlev@gmail.com>
2022-10-17 22:55:23 +0300

    0 task

--- src/queue.js
+++ src/queue.js
@@ -17,7 +17,9 @@ class Queue {
 
   getUnderlyingList() {
     throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    
+
+    
   }
 
   enqueue(/* value */) {
